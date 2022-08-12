007ba7dd1f0579fb3c9900d7bf07c620b7604f6e
harshavardhan reddy <86282138+Harshavardhan200@users.noreply.github.com>
2022-08-12 23:53:35 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,6 +1,5 @@
 from locations import *
 import time
-list1 = [(15.7981369807174, 78.07836403585469), (15.79729668063069, 78.07760405129781), (15.797241787632668, 78.0775743578153), (15.796960621390312, 78.07747336628034), (15.796924341846738, 78.07767400279646), (15.797135540526392, 78.07700409222842), (15.79687575315547, 78.07691858603702), (15.796631513856962, 78.0768579911135)]
 lcd_ = LCD()
 lcd_.clear_the_screen()
 last_location = "System Rebooted"
@@ -19,7 +18,7 @@ while True:
         if location != last_location:
             last_location=location
             Sound.audio("Connecting..")
-            break
+            continue
    if location != last_location:
         Sound.audio(location)
         lcd_.display(location)
